3bcb6a7a8d53a0789ca020c304d0549b9a31a77b
Guest User <Guest@a-05-08-lab.mddn.vuw.ac.nz>
2017-08-20 14:48:41 +1200

    Updated script to scrape images from different years

--- video/get-cage.sh
+++ video/get-cage.sh
@@ -1,22 +1,24 @@
 set -ex
-for PAGE in $(seq -f "%02g" 1 12)
+for YEAR in 2009 2010 2011 2013
 do
-# build the url 
-URL="http://niccageaseveryone.blogspot.co.nz/2010/"$PAGE
-	echo $URL
-	# fetch the images \
-	wget \
-		--adjust-extension \
-		--no-directories \
-		--convert-links \
-		--backup-converted \
-		--random-wait\
-		--limit-rate=100k \
-		--span-hosts \
-		--directory-prefix=temp/downloads \
-		--page-requisites \
-		--timestamping \
-		--execute robots=off \
-		--accept=*.jpg \
-		$URL
+	for PAGE in $(seq -f "%02g" 1 12)
+	do
+	# build the url 
+	URL="http://niccageaseveryone.blogspot.co.nz/"$YEAR"/"$PAGE
+		# fetch the images \
+		wget \
+			--adjust-extension \
+			--no-directories \
+			--convert-links \
+			--backup-converted \
+			--random-wait\
+			--limit-rate=100k \
+			--span-hosts \
+			--directory-prefix=temp/downloads \
+			--page-requisites \
+			--timestamping \
+			--execute robots=off \
+			--accept=*.jpg \
+			$URL
+	done
 done
